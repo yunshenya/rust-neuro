7dd31d97b5644df264a927ec9c8af7fa33f21f70
云深 <yunshen4869@gmail.com>
2024-06-18 20:04:08 +0800

    外挂编写例子

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,25 @@
-use std::ffi::CString;
-use std::ptr;
+use crate::activations::SIGMOID;
+use crate::network::Network;
 
-use winapi::shared::minwindef::{FALSE, LPCVOID, LPVOID};
-use winapi::um::memoryapi::{ReadProcessMemory, WriteProcessMemory};
-use winapi::um::processthreadsapi::OpenProcess;
-use winapi::um::winnt::PROCESS_ALL_ACCESS;
-use winapi::um::winuser::{FindWindowA, GetWindowThreadProcessId};
+pub mod activations;
+pub mod matrix;
+pub mod network;
 
 fn main() {
-    let windows_name = CString::new("植物大战僵尸").unwrap();
-    unsafe {
-        let handle = FindWindowA(ptr::null(), windows_name.as_ptr());
-        if handle.is_null() {
-            println!("窗口没有找到");
-        }
-        let mut pid:u32 = 0;
-        GetWindowThreadProcessId(handle, &mut pid as *mut u32);
-        let hprocess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid);
-        let base_adr = 0x006A9EC0 as LPCVOID;
-        let offsets = [0x768, 0x5560];
-        let value: u32 = 0;
-        let tmp: usize = 0;
-        let tmp2: usize = 0;
+    let inputs = vec![
+        vec![0.0, 0.0],
+        vec![0.0, 1.0],
+        vec![1.0, 0.0],
+        vec![1.0, 1.0],
+    ];
+    let targets = vec![vec![0.0], vec![1.0], vec![1.0], vec![0.0]];
 
-        ReadProcessMemory(hprocess, base_adr, tmp as LPVOID, size_of_val(&tmp), ptr::null_mut());
-        ReadProcessMemory(hprocess, (tmp + offsets[0]) as LPCVOID, tmp2 as LPVOID, size_of_val(&tmp2), ptr::null_mut());
-        ReadProcessMemory(hprocess, (tmp2  + offsets[1]) as LPCVOID, value  as LPVOID, size_of_val(&value), ptr::null_mut());
-        println!("阳光的值：{}", value);
+    let mut network = Network::new(vec![2, 3, 1], 0.5, SIGMOID);
 
-        let lock_value: u32 = 9999;
-        WriteProcessMemory(hprocess, (tmp2 + offsets[1]) as LPVOID, lock_value as LPCVOID, size_of_val(&lock_value), ptr::null_mut());
-    }
+    network.train(inputs, targets, 2000);
+
+    println!("{:?}", network.feed_forward(vec![0.0, 0.0]).get(0).unwrap().round());
+    println!("{:?}", network.feed_forward(vec![0.0, 1.0]).get(0).unwrap().round());
+    println!("{:?}", network.feed_forward(vec![1.0, 0.0]).get(0).unwrap().round());
+    println!("{:?}", network.feed_forward(vec![1.0, 1.0]).get(0).unwrap().round());
 }
---
+++ src/network.rs
@@ -0,0 +1,134 @@
+use std::{
+    fs::File,
+    io::{Read, Write},
+};
+
+use serde::{Deserialize, Serialize};
+use serde_json::{from_str, json};
+
+use super::{activations::Activation, matrix::Matrix};
+
+pub struct Network<'a> {
+    layers: Vec<usize>,
+    weights: Vec<Matrix>,
+    biases: Vec<Matrix>,
+    data: Vec<Matrix>,
+    learning_rate: f64,
+    activation: Activation<'a>,
+}
+
+#[derive(Serialize, Deserialize)]
+struct SaveData {
+    weights: Vec<Vec<Vec<f64>>>,
+    biases: Vec<Vec<Vec<f64>>>,
+}
+
+impl Network<'_> {
+    pub fn new(
+        layers: Vec<usize>,
+        learning_rate: f64,
+        activation: Activation,
+    ) -> Network {
+        let mut weights = vec![];
+        let mut biases = vec![];
+
+        for i in 0..layers.len() - 1 {
+            weights.push(Matrix::random(layers[i + 1], layers[i]));
+            biases.push(Matrix::random(layers[i + 1], 1));
+        }
+
+        Network {
+            layers,
+            weights,
+            biases,
+            data: vec![],
+            learning_rate,
+            activation,
+        }
+    }
+
+    pub fn feed_forward(&mut self, inputs: Vec<f64>) -> Vec<f64> {
+        if inputs.len() != self.layers[0] {
+            panic!("Invalid inputs length");
+        }
+
+        let mut current = Matrix::from(vec![inputs]).transpose();
+        self.data = vec![current.clone()];
+
+        for i in 0..self.layers.len() - 1 {
+            current = self.weights[i]
+                .multiply(&current)
+                .add(&self.biases[i])
+                .map(self.activation.function);
+            self.data.push(current.clone());
+        }
+
+        current.transpose().data[0].to_owned()
+    }
+
+    pub fn back_propagate(&mut self, outputs: Vec<f64>, targets: Vec<f64>) {
+        if targets.len() != self.layers[self.layers.len() - 1] {
+            panic!("Invalid targets length");
+        }
+
+        let parsed = Matrix::from(vec![outputs]).transpose();
+        let mut errors = Matrix::from(vec![targets]).transpose().subtract(&parsed);
+        let mut gradients = parsed.map(self.activation.derivative);
+
+        for i in (0..self.layers.len() - 1).rev() {
+            gradients = gradients
+                .dot_multiply(&errors)
+                .map(&|x| x * self.learning_rate);
+
+            self.weights[i] = self.weights[i].add(&gradients.multiply(&self.data[i].transpose()));
+            self.biases[i] = self.biases[i].add(&gradients);
+
+            errors = self.weights[i].transpose().multiply(&errors);
+            gradients = self.data[i].map(self.activation.derivative);
+        }
+    }
+
+    pub fn train(&mut self, inputs: Vec<Vec<f64>>, targets: Vec<Vec<f64>>, epochs: u16) {
+        for i in 1..=epochs {
+            if epochs < 100 || i % (epochs / 100) == 0 {
+                println!("Epoch {} of {}", i, epochs);
+            }
+            for j in 0..inputs.len() {
+                let outputs = self.feed_forward(inputs[j].clone());
+                self.back_propagate(outputs, targets[j].clone());
+            }
+        }
+    }
+
+    pub fn save(&self, file: String) {
+        let mut file = File::create(file).expect("Unable to touch save file");
+
+        file.write_all(
+            json!({
+                "weights": self.weights.clone().into_iter().map(|matrix| matrix.data).collect::<Vec<Vec<Vec<f64>>>>(),
+                "biases": self.biases.clone().into_iter().map(|matrix| matrix.data).collect::<Vec<Vec<Vec<f64>>>>()
+            }).to_string().as_bytes(),
+        ).expect("Unable to write to save file");
+    }
+
+    pub fn load(&mut self, file: String) {
+        let mut file = File::open(file).expect("Unable to open save file");
+        let mut buffer = String::new();
+
+        file.read_to_string(&mut buffer)
+            .expect("Unable to read save file");
+
+        let save_data: SaveData = from_str(&buffer).expect("Unable to serialize save data");
+
+        let mut weights = vec![];
+        let mut biases = vec![];
+
+        for i in 0..self.layers.len() - 1 {
+            weights.push(Matrix::from(save_data.weights[i].clone()));
+            biases.push(Matrix::from(save_data.biases[i].clone()));
+        }
+
+        self.weights = weights;
+        self.biases = biases;
+    }
+}

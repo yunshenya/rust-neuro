e3671d9edbb8a66e11c2b2506d4b2cf5ea81ab7f
云深 <yunshen4869@gmail.com>
2024-06-28 12:34:06 +0800

    添加注解

--- src/matrix.rs
+++ src/matrix.rs
@@ -1,6 +1,7 @@
 use rand::{thread_rng, Rng};
 use std::fmt::{Debug, Formatter, Result};
 
+// 定义矩阵结构体
 #[derive(Clone)]
 pub struct Matrix {
     pub rows: usize,
@@ -9,6 +10,7 @@ pub struct Matrix {
 }
 
 impl Matrix {
+    /// 创建一个rows行cols列的全零矩阵
     pub fn zeros(rows: usize, cols: usize) -> Matrix {
         Matrix {
             rows,
@@ -17,20 +19,28 @@ impl Matrix {
         }
     }
 
+    /// 创建一个指定大小的随机矩阵
     pub fn random(rows: usize, cols: usize) -> Matrix {
+        // 获取当前线程的随机数生成器
         let mut rng = thread_rng();
 
+        // 创建一个全零矩阵
         let mut res = Matrix::zeros(rows, cols);
+        // 遍历矩阵的每个元素
         for i in 0..rows {
             for j in 0..cols {
+                // 生成一个-1到1之间的随机数
                 res.data[i][j] = rng.gen::<f64>() * 2.0 - 1.0;
             }
         }
 
+        // 返回生成的随机矩阵
         res
     }
 
+     // 此函数接受 f64 的向量向量并返回一个 Matrix
     pub fn from(data: Vec<Vec<f64>>) -> Matrix {
+        // 使用输入数据的维度创建一个新矩阵
         Matrix {
             rows: data.len(),
             cols: data[0].len(),
@@ -38,59 +48,83 @@ impl Matrix {
         }
     }
 
+    /// 实现矩阵乘法
     pub fn multiply(&self, other: &Matrix) -> Matrix {
+        // 如果矩阵的列数不相等，则抛出异常
         if self.cols != other.rows {
             panic!("Attempted to multiply by matrix of incorrect dimensions");
         }
 
+        // 创建一个结果矩阵，并用0填充
         let mut res = Matrix::zeros(self.rows, other.cols);
 
+        // 遍历self的行数和other的列数
         for i in 0..self.rows {
             for j in 0..other.cols {
+                // 初始化一个变量，用于存储乘积
                 let mut sum = 0.0;
+
+                // 遍历self的列数和other的行数
                 for k in 0..self.cols {
                     sum += self.data[i][k] * other.data[k][j];
                 }
 
+                // 将乘积赋值给结果矩阵
                 res.data[i][j] = sum;
             }
         }
 
+        // 返回结果矩阵
         res
     }
 
+    /// 实现矩阵的加法
     pub fn add(&self, other: &Matrix) -> Matrix {
+        // 如果矩阵的行列数不同，则抛出异常
         if self.rows != other.rows || self.cols != other.cols {
             panic!("Attempted to add matrix of incorrect dimensions");
         }
 
+        // 创建一个新的矩阵，元素全为0
         let mut res = Matrix::zeros(self.rows, self.cols);
 
+        // 遍历矩阵的每一个元素，将两个矩阵对应元素相加
         for i in 0..self.rows {
             for j in 0..self.cols {
                 res.data[i][j] = self.data[i][j] + other.data[i][j];
             }
         }
 
+        // 返回结果矩阵
         res
     }
 
+    /// 矩阵点乘
     pub fn dot_multiply(&self, other: &Matrix) -> Matrix {
+        // 检查两个矩阵的维度是否相同
         if self.rows != other.rows || self.cols != other.cols {
             panic!("Attempted to dot multiply by matrix of incorrect dimensions");
         }
 
+        // 创建一个结果矩阵，初始化为0
         let mut res = Matrix::zeros(self.rows, self.cols);
 
+        // 遍历矩阵的每个元素，进行点乘
         for i in 0..self.rows {
             for j in 0..self.cols {
                 res.data[i][j] = self.data[i][j] * other.data[i][j];
             }
         }
 
+        // 返回结果矩阵
         res
     }
 
+    /// Subtracts two matrices element-wise.
+    ///
+    /// # Panics
+    ///
+    /// Panics if the matrices have different dimensions.
     pub fn subtract(&self, other: &Matrix) -> Matrix {
         if self.rows != other.rows || self.cols != other.cols {
             panic!("Attempted to subtract matrix of incorrect dimensions");
@@ -107,43 +141,72 @@ impl Matrix {
         res
     }
 
+      /// Applies a function to each element of the matrix and returns a new matrix with the results.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use matrix::Matrix;
+    ///
+    /// let m = Matrix::from([[1.0, 2.0], [3.0, 4.0]]);
+    /// let doubled = m.map(|x| x * 2.0);
+    ///
+    /// assert_eq!(doubled, Matrix::from([[2.0, 4.0], [6.0, 8.0]]);
+    /// ```
     pub fn map(&self, function: &dyn Fn(f64) -> f64) -> Matrix {
+        // Creates a new matrix from a two-dimensional vector of floating-point numbers.
         Matrix::from(
             self.data
                 .clone()
                 .into_iter()
+                //  Maps each element of the row to the result of applying the function.
                 .map(|row| row.into_iter().map(|value| function(value)).collect())
+                // Collects the rows into a two-dimensional vector.
                 .collect(),
         )
     }
 
     pub fn transpose(&self) -> Matrix {
+        // 创建一个新的矩阵，行列互换
         let mut res = Matrix::zeros(self.cols, self.rows);
 
+        // 遍历原矩阵
         for i in 0..self.rows {
             for j in 0..self.cols {
+                // 将原矩阵的元素赋值给新矩阵
                 res.data[j][i] = self.data[i][j];
             }
         }
 
+        // 返回新矩阵
         res
     }
 }
 
 impl Debug for Matrix {
+    // 实现Debug trait的fmt方法
     fn fmt(&self, f: &mut Formatter) -> Result {
+        // 写入格式化字符串
         write!(
             f,
             "Matrix {{\n{}\n}}",
             (&self.data)
+                // 遍历矩阵的每一行
                 .into_iter()
+                // 将每一行转换为字符串
                 .map(|row| "  ".to_string()
                     + &row
+                        // 遍历每一行的元素
                         .into_iter()
+                        // 将元素转换为字符串
                         .map(|value| value.to_string())
+                        // 将字符串元素连接成一个字符串
                         .collect::<Vec<String>>()
+                        // 将字符串元素以空格分隔
                         .join(" "))
+                // 将每一行的字符串连接成一个字符串
                 .collect::<Vec<String>>()
+                // 将每一行的字符串以换行符分隔
                 .join("\n")
         )
     }
--- src/network.rs
+++ src/network.rs
@@ -8,6 +8,7 @@ use serde_json::{from_str, json};
 
 use super::{activations::Activation, matrix::Matrix};
 
+// 定义网络结构体
 pub struct Network<'a> {
     layers: Vec<usize>,
     weights: Vec<Matrix>,
@@ -17,17 +18,22 @@ pub struct Network<'a> {
     activation: Activation<'a>,
 }
 
+// 定义一个结构体SaveData，用于存储权重和偏置
 #[derive(Serialize, Deserialize)]
 struct SaveData {
+    // 存储权重
     weights: Vec<Vec<Vec<f64>>>,
+    // 存储偏置
     biases: Vec<Vec<Vec<f64>>>,
 }
 
 impl Network<'_> {
+    ///使用给定的层、学习率和激活函数创建一个新的神经网络。
     pub fn new(layers: Vec<usize>, learning_rate: f64, activation: Activation) -> Network {
         let mut weights = vec![];
         let mut biases = vec![];
 
+        // Generate random weights and biases for each layer.
         for i in 0..layers.len() - 1 {
             weights.push(Matrix::random(layers[i + 1], layers[i]));
             biases.push(Matrix::random(layers[i + 1], 1));
@@ -43,35 +49,50 @@ impl Network<'_> {
         }
     }
 
+    /// 根据输入，前向传播神经网络，并返回输出
     pub fn feed_forward(&mut self, inputs: Vec<f64>) -> Vec<f64> {
+        // 检查输入长度是否正确
         if inputs.len() != self.layers[0] {
             panic!("Invalid inputs length");
         }
 
+        // 将输入转换为矩阵，并将其转置
         let mut current = Matrix::from(vec![inputs]).transpose();
+        // 将当前矩阵放入数据列表中
         self.data = vec![current.clone()];
 
+        // 遍历网络的所有层，除了最后一层
         for i in 0..self.layers.len() - 1 {
+            // 将当前矩阵乘以权重矩阵，并加上偏置矩阵，然后应用激活函数
             current = self.weights[i]
                 .multiply(&current)
                 .add(&self.biases[i])
                 .map(self.activation.function);
+            // 将当前矩阵放入数据列表中
             self.data.push(current.clone());
         }
 
+        // 将输出矩阵转置，并返回输出向量
         current.transpose().data[0].to_owned()
     }
 
+    /// 根据输出和目标，反向传播神经网络
     pub fn back_propagate(&mut self, outputs: Vec<f64>, targets: Vec<f64>) {
+        // 检查目标长度是否正确
         if targets.len() != self.layers[self.layers.len() - 1] {
             panic!("Invalid targets length");
         }
 
+        // 将输出转换为矩阵，并将其转置
         let parsed = Matrix::from(vec![outputs]).transpose();
+        // 计算输出误差
         let mut errors = Matrix::from(vec![targets]).transpose().subtract(&parsed);
+        // 计算输出梯度
         let mut gradients = parsed.map(self.activation.derivative);
 
+        // 遍历网络的所有层，从最后一层开始
         for i in (0..self.layers.len() - 1).rev() {
+            // 计算权重梯度和偏置梯度
             gradients = gradients
                 .dot_multiply(&errors)
                 .map(&|x| x * self.learning_rate);
@@ -79,26 +100,40 @@ impl Network<'_> {
             self.weights[i] = self.weights[i].add(&gradients.multiply(&self.data[i].transpose()));
             self.biases[i] = self.biases[i].add(&gradients);
 
+            // 计算误差和梯度，用于下一层
             errors = self.weights[i].transpose().multiply(&errors);
             gradients = self.data[i].map(self.activation.derivative);
         }
     }
 
+    /// 使用一组输入和目标训练神经网络。
+    ///
+    /// # Arguments
+    /// * `inputs` - 输入向量的向量。
+    /// * `targets` -目标向量的向量。
+    /// * `epochs` - 运行训练数据的次数。
     pub fn train(&mut self, inputs: Vec<Vec<f64>>, targets: Vec<Vec<f64>>, epochs: u16) {
+        // 循环遍历 epoch 数。
         for i in 1..=epochs {
             if epochs < 100 || i % (epochs / 100) == 0 {
                 println!("Epoch {} of {}", i, epochs);
             }
+            //遍历每个输入向量。
             for j in 0..inputs.len() {
+                //  通过网络向前馈送输入向量并存储输出。
                 let outputs = self.feed_forward(inputs[j].clone());
+                //  通过网络将错误传播回去。
                 self.back_propagate(outputs, targets[j].clone());
             }
         }
     }
 
+    /// 保存神经网络到文件
     pub fn save(&self, file: String) {
+        // 创建文件
         let mut file = File::create(file).expect("Unable to touch save file");
 
+        // 将权重和偏置写入文件
         file.write_all(
             json!({
                 "weights": self.weights.clone().into_iter().map(|matrix| matrix.data).collect::<Vec<Vec<Vec<f64>>>>(),
@@ -107,23 +142,30 @@ impl Network<'_> {
         ).expect("Unable to write to save file");
     }
 
+    /// 从文件加载神经网络
     pub fn load(&mut self, file: String) {
+        // 打开文件
         let mut file = File::open(file).expect("Unable to open save file");
         let mut buffer = String::new();
 
+        // 读取文件内容
         file.read_to_string(&mut buffer)
             .expect("Unable to read save file");
 
+        // 将文件内容反序列化为SaveData结构体
         let save_data: SaveData = from_str(&buffer).expect("Unable to serialize save data");
 
+        // 创建权重和偏置向量
         let mut weights = vec![];
         let mut biases = vec![];
 
+        // 从SaveData结构体中取出权重和偏置，并转换为矩阵
         for i in 0..self.layers.len() - 1 {
             weights.push(Matrix::from(save_data.weights[i].clone()));
             biases.push(Matrix::from(save_data.biases[i].clone()));
         }
 
+        // 更新神经网络的权重和偏置
         self.weights = weights;
         self.biases = biases;
     }
